bd870ee9f019cd22ff117976eb287935d749778e
rex <rexlimpangog773@gmail.com>
2024-05-29 11:49:47 +0800

    can now load data

--- app/data.py
+++ app/data.py
@@ -1,62 +1,61 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
-from flask import current_app as app
 from . import config
 
 data_fetch = Blueprint('data', __name__)
+from . import config
 
-@data_fetch.route('/records_data', methods=['GET', 'POST'])
-@login_required
+
+@data_fetch.route("/records_data",methods=["POST","GET"])
 def records_data():
-    if current_user.is_authenticated:
-        cursor = config.conn.cursor()
-        
-        if request.method =='POST':
-            draw = request.form['draw']
-            row = int(request.form['start'])
-            rowperpage = int(request.form['length'])
-            column_index = request.form['order[0][column]']
-            sort_direction = request.form['order[0][dir]']
-            searchValue = request.form['search[value]']
-            likeString = "%" + searchValue + "%"
-            
-            # Total count data
-            cursor.execute('SELECT COUNT(*) AS ALLCOUNT FROM srecordstbl' )
-            rsallcounts = cursor.fetchone()
-            totalRecords = rsallcounts['ALLCOUNT']
-            
-            # Total count with filter
-            cursor.execute('SELECT COUNT(*) AS ALLCOUNT FROM srecordstbl WHERE FullName LIKE %s OR College LIKE %s OR Course LIKE %s OR year_level LIKE %s OR Subject LIKE %s OR Semester LIKE %s OR SchoolYear LIKE %s', (likeString, likeString, likeString, likeString, likeString, likeString, likeString))
-            rsallcounts = cursor.fetchone()
-            totalRecordswFilter = rsallcounts['ALLCOUNT']
-            
-            # feth all data records
-            if searchValue == '':   
-                cursor.execute(f'SELECT * FROM srecordstbl ORDER BY {column_index} {sort_direction} LIMIT %s, %s;', (row, rowperpage))
-                student_records = cursor.fetchall()
-            else:
-                cursor.execute(f'SELECT * FROM srecordstbl WHERE FullName LIKE %s OR College LIKE %s OR Course LIKE %s OR year_level LIKE %s OR Subject LIKE %s OR Semester LIKE %s OR SchoolYear LIKE %s ORDER BY {column_index} {sort_direction} LIMIT %s, %s;', (likeString, likeString, likeString, likeString, likeString, likeString, likeString, row, rowperpage))
-                student_records = cursor.fetchall()
-
-            # Convert your data to a format that DataTables can read
-            data = []
-            for row in student_records:
-                data.append({ 
-                            'id' : row['id'],
-                            'FullName' : row['FullName'],
-                            'College' : row['College'],
-                            'Course' : row['Course'],
-                            'year_level' : row['year_level'],
-                            'Subject' : row['Subject'],
-                            'Semester' : row['Semester'],
-                            'SchoolYear' : row['SchoolYear'] 
-                            })
-
-            response = {
-                'draw': draw,
-                'iTotalRecords': totalRecords,
-                'iTotalDisplayRecords': totalRecordswFilter,
-                'aaData': data,
+    try:
+        with config.conn.cursor() as cursor:
+            if request.method == 'POST':
+                draw = request.form['draw'] 
+                row = int(request.form['start'])
+                rowperpage = int(request.form['length'])
+                searchValue = request.form["search[value]"]
+ 
+                ## Total number of records without filtering
+                cursor.execute("select count(*) as allcount from srecordstbl")
+                rsallcount = cursor.fetchone()
+                totalRecords = rsallcount['allcount']
+ 
+                ## Total number of records with filtering
+                likeString = "%" + searchValue +"%"
+                cursor.execute("SELECT count(*) as allcount from srecordstbl WHERE FullName LIKE %s OR College LIKE %s OR Course LIKE %s", (likeString, likeString, likeString))
+                rsallcount = cursor.fetchone()
+                totalRecordwithFilter = rsallcount['allcount']
+ 
+                ## Fetch records
+                if searchValue=='':
+                    cursor.execute("SELECT * FROM srecordstbl ORDER BY FullName asc limit %s, %s;", (row, rowperpage))
+                    recordlist = cursor.fetchall()
+                else:        
+                    cursor.execute("SELECT * FROM srecordstbl WHERE FullName LIKE %s OR College LIKE %s OR Course LIKE %s limit %s, %s;", (likeString, likeString, likeString, row, rowperpage))
+                    recordlist = cursor.fetchall()
+ 
+                data = []
+                for row in recordlist:
+                    data.append({
+                        'id': row['id'],
+                        'FullName': row['FullName'],
+                        'College': row['College'],
+                        'Course': row['Course'],
+                        'Section': row['Section'],
+                        'year_level': row['year_level'],
+                        'Subject': row['Subject'],
+                        'Unit': row['Unit'],
+                        'Semester': row['Semester'],
+                        'SchoolYear': row['SchoolYear'],
+                    })
+ 
+                response = {
+                    'draw': draw,
+                    'iTotalRecords': totalRecords,
+                    'iTotalDisplayRecords': totalRecordwithFilter,
+                    'aaData': data,
                 }
-            
-            return jsonify(response)
+                return jsonify(response)
+    except Exception as e:
+        print(e)
