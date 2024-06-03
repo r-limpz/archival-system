0ad7c8f127b01698eaf0cdf47143a8fd6ae5149e
rex <rexlimpangog773@gmail.com>
2024-06-03 16:23:37 +0800

    add login attemp not in server

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, request, Blueprint, jsonify, session
 from flask_login import UserMixin, login_user, login_required, logout_user, current_user
+import datetime
 import socket
 import httpagentparser
 from device_detector import DeviceDetector
@@ -13,21 +14,27 @@ from .forms import LoginForm
 auth = Blueprint('auth', __name__)
 
 logged_devices = []
+blocklist = []
 MAX_ATTEMPTS = 10
+BLOCK_THRESHOLD = datetime.timedelta(hours=1) 
 
+#generate id random character string
 def generate_id(length=256):
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(secrets.choice(characters) for i in range(length))
 
+#generate token based on data, key and key2 for session
 def generate_token(data, key, key2):
     combined_data = str(data) + app.config['SECRET_KEY'] + key + key2
     hashed_data = hashlib.sha256(combined_data.encode()).hexdigest()
     return hashed_data
 
+#verify the token and return true
 def check_token(token, data, key, key2):
     expected_token = generate_token(data, key, key2)
     return token == expected_token
 
+#generate device information of user
 def getUserInfo():
     device_data = {'device': '', 'os': '', 'browser': '', 'ip_address': ''}
     ip_address = socket.gethostbyname(socket.gethostname())
@@ -37,28 +44,60 @@ def getUserInfo():
     browser_name = browser_info.get('browser', {}).get('name', 'Unknown')
     device = DeviceDetector(agent).parse()
     deviceType= device.device_type()
-    os = f"{device.os_name()} {device.os_version()}"  # Improved string formatting
+    os = f"{device.os_name()} {device.os_version()}" 
 
     device_data = {'device': (deviceType), 'os': os, 'browser': browser_name, 'ip_address': ip_address}
     return device_data
 
+#get the user from the array
+def find_device(user):
+    for device in logged_devices:
+        if device['user'] == user:
+            return device
+    return None
+
+#determine if user is blocked and remove if time exceeds the threshold
+def is_blocked(user):
+    current_time = datetime.datetime.now()
+    existing_device = find_device(user)
+    for device in blocklist:
+        if device['user'] == user:
+            if current_time - device['blocked_datetime'] < BLOCK_THRESHOLD:
+                return True
+            else:
+                blocklist.remove(device)
+                if existing_device:
+                    existing_device['attempts'] = 0
+                return False
+    return False
+
+#limit the unsuccessful login attempts request of an ip address 
 def loginAttempt(action):
     user_info = getUserInfo()
     ipaddress = user_info['ip_address']
-    device = (hashlib.sha256(ipaddress.encode()).hexdigest())
-    existing_device = next((device for device in logged_devices if device['mkasan302923439hsabah'] == device), None)
+    user = hashlib.sha256(ipaddress.encode()).hexdigest()
+    existing_device = find_device(user)
+    
+    if is_blocked(user):
+        print( ipaddress," is currently blocked from making login attempts.")
+    
+    else:
+        if action == 'reset':
+            if existing_device:
+                existing_device['attempts'] = 0
+            else:
+                logged_devices.append({'user': user, 'attempts': 0})
 
-    if action == 'reset':
-        if existing_device:
-            existing_device['count'] = 0
-        else:
-            logged_devices.append({'mkasan302923439hsabah': device, 'count': 0})
-    elif action == 'attempt':
-        if existing_device:
-            existing_device['count'] += 1
-        else:
-            logged_devices.append({'mkasan302923439hsabah': device, 'count': 1})
+        elif action == 'count':
+            if existing_device:
+                existing_device['attempts'] += 1
+                if existing_device['attempts'] >= MAX_ATTEMPTS:
+                    if not any(device['user'] == user for device in blocklist):
+                        blocklist.append({'user': user, 'blocked_datetime': datetime.datetime.now()})
+            else:
+                logged_devices.append({'user': user, 'attempts': 1})
 
+#generate a user device information and append to the database login history
 def loginHistory(user_id, session_data):
     with config.conn.cursor() as cursor:
         user_deviceInfo = getUserInfo()
@@ -75,6 +114,7 @@ def loginHistory(user_id, session_data):
                            (str(user_id), device, browser, os, ip_address, session_data))
             config.conn.commit()
 
+#setup query to update database when user session expired
 def session_expired(username):
     if username:
         with config.conn.cursor() as cursor:
@@ -92,6 +132,8 @@ def session_expired(username):
                     print('session user not found')
             else:
                 print('session user not found')
+
+#setup for user session
 class User(UserMixin):
     def __init__(self, id, username, role, token):
         self.id = id
@@ -99,6 +141,7 @@ class User(UserMixin):
         self.role = role
         self.token = token
 
+    #fetch user information based on input
     def get_User(self):
         with config.conn.cursor() as cursor:
             cursor.execute('SELECT * FROM session WHERE session_id = %s', (self.id,))
@@ -109,7 +152,8 @@ class User(UserMixin):
                 return cursor.fetchone()
             else:
                 return None
-                
+            
+    #verify user session token authentication           
     @property
     def is_authenticated(self):
         with config.conn.cursor() as cursor:
@@ -120,6 +164,8 @@ class User(UserMixin):
                 return True
             else:
                 return False
+            
+    #verify if user account is not deactivated
     @property
     def is_active(self):
         user = self.get_User()
@@ -127,7 +173,8 @@ class User(UserMixin):
             return True
         else:
             return False
-
+        
+#load user account in session
 @login_manager.user_loader
 def load_user(session_id):
     try:
@@ -150,16 +197,17 @@ def load_user(session_id):
     except Exception as e:
         print(f"login loader: {e}")
 
+#login user
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     role = ''
     invalidError = 'User not Found or Invalid Password. Please Try Again!'
-    captchaError = 'To confirm that you’re a person and not a robot, solve the captcha.'
+    captchaError = 'Please solve the captcha to verify you’re not a robot. Please Try Again!'
+    attemptError = 'Excessive login attempts detected. Your account is temporarily blocked for 1 hour. Please try again later.'
     error = invalidError
     
     if form.validate_on_submit():
-            #setup variables from input fields
         username = request.form.get('username')
         password = request.form.get('password')
         role = request.form.get('role')
@@ -167,48 +215,54 @@ def login():
         form.captcha.data = ''
         user_role = {'1': 'admin', '2': 'staff'}.get(role, 'error')
         role = int(role)
+        
+        user_info = getUserInfo()
+        ipaddress = user_info['ip_address']
+        device = hashlib.sha256(ipaddress.encode()).hexdigest()
 
-        with config.conn.cursor() as cursor:
-            #Search for the user in the database
-            cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
-            user = cursor.fetchone()
+        if is_blocked(device):
+                error = attemptError
+        else:
+            with config.conn.cursor() as cursor:
+                #Search for the user in the database
+                cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
+                user = cursor.fetchone()
 
-            if captcha.get_answer() == input_captcha:#validate captcha input
-                if user and argon2.check_password_hash(user['password'], password) and user['status'] == 1:#authentication for login
+                if captcha.get_answer() == input_captcha:#validate captcha input
+                    if user and argon2.check_password_hash(user['password'], password) and user['status'] == 1:#authentication for login
 
-                    cursor.execute('SELECT * FROM session WHERE user_id = %s', (user['user_id']))
-                    session_data = cursor.fetchone()
+                        cursor.execute('SELECT * FROM session WHERE user_id = %s', (user['user_id']))
+                        session_data = cursor.fetchone()
 
-                    if session_data and len(session_data['session_id']) == 256:
-                        session_id = session_data['session_id']
-                    else:
-                        session_id = generate_id()
-                        cursor.execute('INSERT INTO session(session_id, user_id, username, role) VALUES (%s,%s,%s,%s)', (session_id, user['user_id'], username, role))
-                        config.conn.commit()
-                        
-                    if session_id:
-                        #generate token for session creation on cookies
-                        token = generate_token(user['password'], user['pass_key'], session_id)
-                        login_user(User(session_id, username, user_role, token), remember=False)
-                        loginHistory((user['user_id']), session_id)
-                        loginAttempt('reset')
-                        redirect_url = {1: 'admin.dashboard', 2: 'staff.records'}.get(user['role'], 'auth.logout')
+                        if session_data and len(session_data['session_id']) == 256:
+                            session_id = session_data['session_id']
+                        else:
+                            session_id = generate_id()
+                            cursor.execute('INSERT INTO session(session_id, user_id, username, role) VALUES (%s,%s,%s,%s)', (session_id, user['user_id'], username, role))
+                            config.conn.commit()
+                            
+                        if session_id:
+                            #generate token for session creation on cookies
+                            token = generate_token(user['password'], user['pass_key'], session_id)
+                            login_user(User(session_id, username, user_role, token), remember=False)
+                            loginHistory((user['user_id']), session_id)
+                            loginAttempt('reset')
+                            redirect_url = {1: 'admin.dashboard', 2: 'staff.records'}.get(user['role'], 'auth.logout')
 
-                        if redirect_url:#redirect if user is authenicated and authorized
-                            return redirect(url_for(redirect_url))
+                            if redirect_url:#redirect if user is authenicated and authorized
+                                return redirect(url_for(redirect_url))
+                            else:
+                                error = invalidError
                         else:
                             error = invalidError
                     else:
                         error = invalidError
                 else:
-                    error = invalidError
-                    loginAttempt('attempt')
-                    print(logged_devices)
-            else:
-                error = captchaError
-               
+                    error = captchaError
+    loginAttempt('count')           
     return render_template('public/index.html', error_message=error, role = role, form=form)
 
+#logout user
 @auth.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
@@ -228,6 +282,17 @@ def logout():
 
     return redirect(url_for('home'))
 
+
+
+
+
+
+
+
+
+
+
+#check for session timeout
 @auth.route('/get_heartbeat/<username>', endpoint='heartbeat')
 def heartbeat(username):
     if current_user:
@@ -241,7 +306,8 @@ def heartbeat(username):
     else:
         session_expired(username)
         return jsonify(session_Inactive = True)
-
+    
+#return to logout to prevent rendering expired csrf token
 @auth.route('/authenticate-user/check-token/timeout/')
 def user_timeout():
     return redirect(url_for('auth.logout'))
