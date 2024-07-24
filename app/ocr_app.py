e04b200eb5d2fae71fceea555154929b3bfb7a89
rex <rexlimpangog773@gmail.com>
2024-07-24 14:47:57 +0800

    added auto-scan using paddleOCR and manual scan with tesseract

--- app/ocr_app.py
+++ app/ocr_app.py
@@ -1,8 +1,11 @@
 from flask import current_app as app
 from flask import Blueprint, request, jsonify
 from flask_login import login_required
+import pytesseract
+from PIL import Image
 from app.analyzer.detectTable import CropTable
-from app.analyzer.text_recognition import ocr_scanner
+from app.analyzer.tableRecognition import tableDataAnalyzer
+from app.analyzer.name_formatter import detectStudentNames
 
 ocr_App = Blueprint('ocr', __name__)
 
@@ -16,9 +19,9 @@ class StudentNames:
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-@ocr_App.route('/scanner', methods=['POST'])
+@ocr_App.route('/scanner/auto/<auto>', methods=['POST'])
 @login_required
-def scanner():
+def scanner(auto):
 
     if 'document_image' not in request.files:
             return "No file uploaded"
@@ -29,11 +32,24 @@ def scanner():
             return "No file selected or unsupported file type"
     
     try:
-        crop_image = CropTable(file)
-        if crop_image:
-            students = ocr_scanner(crop_image)
+        if auto == "true":
+                print("auto-scan")
+                crop_image = CropTable(file)
+
+                if crop_image:
+                    students = tableDataAnalyzer()
+                
+        else:
+            print("manual-scan")
+            img = Image.open(file)
+            text = pytesseract.image_to_string(img)
+            raw_names = text.split('\n')
+            students = detectStudentNames(raw_names)
             
+        if students:
             return jsonify([student.__dict__ for student in students])
+        else:
+            return None
         
     except Exception as e:
         return jsonify(e)
