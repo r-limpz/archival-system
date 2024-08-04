25664b76957faaadb06d386b3bd8200a6250663c
rex <rexlimpangog773@gmail.com>
2024-08-04 17:36:27 +0800

    added image background change to white for png image with transparency error, fixed : "cannot write mode RGBA as JPEG"

--- app/analyzer/detectTable.py
+++ app/analyzer/detectTable.py
@@ -2,8 +2,17 @@ import numpy as np
 from PIL import Image
 from ultralyticsplus import YOLO 
 
+def make_white_background(image):
+    # If image has an alpha channel (transparency), convert it to white background
+    if image.mode == 'RGBA':
+        background = Image.new('RGB', image.size, (255, 255, 255))
+        background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+        return background
+    else:
+        return image.convert('RGB')
+
 def CropTable(image, filename_crop):
-    file_path = "./app/analyzer/" + filename_crop # creating the directory
+    file_path = "./app/analyzer/" + filename_crop  # creating the directory
     model = YOLO('foduucom/table-detection-and-extraction')  # Initializing YOLO model with specific weights
 
     # Setting model parameters for object detection
@@ -13,24 +22,25 @@ def CropTable(image, filename_crop):
     model.overrides['max_det'] = 1000  # Maximum number of detections per image
 
     img = Image.open(image)
+    img = make_white_background(img)  # Ensure image has white background if it originally had transparency
+
     results = model.predict(img)  # Execute object detection on the image using the YOLO model
 
     if len(results[0].boxes.data.numpy()) > 0:
         # Extracting coordinates of the first detected bounding box
         x1, y1, x2, y2, _, _ = tuple(int(item) for item in results[0].boxes.data.numpy()[0])
-        img = np.array(Image.open(image))  # Converting the image to a numpy array for processing
+        img_array = np.array(img)  # Convert the image to a numpy array for processing
 
-        # Cropping the image based on the detected bounding box datas
-        cropped_image = img[y1:y2, x1:x2]  # Cropping the image using numpy slicing
+        # Cropping the image based on the detected bounding box data
+        cropped_image = img_array[y1:y2, x1:x2]  # Cropping the image using numpy slicing
         cropped_image = Image.fromarray(cropped_image)  # Converting the cropped numpy array back to PIL Image
 
         try:
             cropped_image.save(file_path)  # save the cropped image
-            return True  # True indicate successful cropping
+            return True  # True indicates successful cropping
         
         except Exception as e:
             print("Error saving cropped image:", e)
             
     print("No table detected in the image.") 
     return False
-
