4eb2c9f4391247f6c9db26ff6dfdff44b38ff7ef
rex <rexlimpangog773@gmail.com>
2024-08-05 17:36:08 +0800

    fixed login page interface bug

--- app/static/js/switch_account.js
+++ app/static/js/switch_account.js
@@ -3,9 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const formTitle = document.getElementById("form-title");
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
+    const captchaImage = document.getElementById("captcha-image");
     const captchaInput = document.getElementById("captcha");
     const errorMessage = document.getElementById("error_message");
     const userRole = document.getElementById("role");
+
+    function clearErrorMessageIfValid() {
+        if (!usernameInput.classList.contains("is-invalid") &&
+            !passwordInput.classList.contains("is-invalid") &&
+            !captchaInput.classList.contains("is-invalid") &&
+            captchaInput.value.length === 6) {
+            errorMessage.innerText = "";
+        }
+    }
 
     switchRolesBtn.addEventListener("click", function () {
 
@@ -13,16 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
             switchRolesBtn.innerText = "Sign in as Staff";
             usernameInput.classList.remove("is-invalid");
             passwordInput.classList.remove("is-invalid");
-            captchaInput.classList.remove("is-invalid");
-            errorMessage.textContent = "";
+            errorMessage.innerText = "";
             formTitle.innerText = "Sign in as Admin";
             userRole.value = '1';
         } else {
             switchRolesBtn.innerText = "Sign in as Admin";
             usernameInput.classList.remove("is-invalid");
             passwordInput.classList.remove("is-invalid");
-            captchaInput.classList.remove("is-invalid");
-            errorMessage.textContent = "";
+            errorMessage.innerText = "";
             formTitle.innerText = "Sign in as Staff";
             userRole.value = '2';
         }
@@ -30,17 +38,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     usernameInput.addEventListener("focus", function () {
         usernameInput.classList.remove("is-invalid");
-        errorMessage.textContent = "";
+        clearErrorMessageIfValid();
     });
 
     passwordInput.addEventListener("focus", function () {
         passwordInput.classList.remove("is-invalid");
-        errorMessage.textContent = "";
+        clearErrorMessageIfValid();
     });
 
-    captchaInput.addEventListener("focus", function () {
-        captchaInput.classList.remove("is-invalid");
-        errorMessage.textContent = "";
+    captchaInput.addEventListener("keyup", function () {
+        if (captchaInput.value.length === 6) {
+            captchaInput.classList.remove("is-invalid");
+            captchaImage.classList.remove("is-invalid");
+            clearErrorMessageIfValid();
+        }
     });
 
 });
