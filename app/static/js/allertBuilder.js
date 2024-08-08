fe3636b56ae6de3672ae92c537b36d09ce45205b
rex <rexlimpangog773@gmail.com>
2024-08-08 21:39:07 +0800

    minor interface changes

--- app/static/js/allertBuilder.js
+++ app/static/js/allertBuilder.js
@@ -6,33 +6,33 @@ function showToast(title, message, theme) {
 
     switch (theme) {
         case 'success':
-            container_theme = "bg-success-subtle";
-            font_color = "text-success";
+            container_theme = "bg-success-subtle opacity-75";
+            font_color = "text-green";
             icon = "fa-solid fa-circle-check";
             break;
         case 'warning':
-            container_theme = "bg-warning-subtle";
+            container_theme = "bg-warning-subtle opacity-75";
             font_color = "text-warning";
             icon = "fa-solid fa-triangle-exclamation";
             break;
         case 'error':
-            container_theme = "bg-danger-subtle";
+            container_theme = "bg-danger-subtle opacity-75";
             font_color = "text-danger";
             icon = "fa-solid fa-circle-xmark";
             break;
     }
 
     let toastContainer = document.getElementById('toastContainer');
-    let toastHTML = `<div class="toast align-items-center ${container_theme}" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="10000" id="add_userToast" style="min-width:400px"> 
-                        <div class="d-flex p-3 gap-2 align-items-center">
-                            <div class="toast-body p-0 flex-grow-1 d-flex justify-content-start gap-3">
+    let toastHTML = `<div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true" id="add_userToast" style="min-width:400px"> 
+                        <div class="d-flex p-2 gap-2 align-items-center">
+                            <div class="toast-body px-2 py-0 flex-grow-1 d-flex justify-content-start gap-3">
                                 <span class="fs-def pt-1 ${font_color} ${icon}"></span>
                                 <div class="">
                                     <div class="fs-def fw-bold ${font_color}"> ${title} </div>
                                     <div class="fs-small"> ${message}</div>
                                 </div>
                             </div>
-                            <button type="button" class="no-border-button fs-medium fs-small bg-transparent p-0" data-bs-dismiss="toast" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
+                            <button type="button" class="no-border-button fs-medium fs-small bg-transparent p-2" data-bs-dismiss="toast" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
                         </div>
                     </div>`;
 
