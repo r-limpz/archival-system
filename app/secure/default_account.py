8f2fb5fe559e3f69bcca0a29f0a23e017fe7d815
Xersii <96998559+rxrlimpz@users.noreply.github.com>
2024-08-27 11:57:22 +0800

    default admin generate

---
+++ app/secure/default_account.py
@@ -0,0 +1,39 @@
+from app.database import config
+from app.secure.randomizer import generate_key
+from app import argon2 
+
+def create_defaultAdmin(password):
+    h_password = argon2.generate_password_hash(password)
+    admin_account = {'username':'admin','fullname':'admin','password':h_password}
+
+    try:
+        with config.conn.cursor() as cursor:
+            cursor.execute('INSERT INTO user (username, fullname, password, pass_key, role, status, online, last_online) VALUES (%s, %s, %s, %s, %s, %s, %s, NULL)', 
+                        (admin_account['username'], admin_account['fullname'], admin_account['password'], generate_key() , 1, 1, 0) )
+            config.conn.commit()
+
+            new_id = cursor.lastrowid
+
+            if new_id:
+                return True
+            
+            return False
+
+    except Exception as e:
+        print(f"create Admin: {e}")
+
+def generateAdmin():
+    try:
+        with config.conn.cursor() as cursor:
+            cursor.execute('SELECT * FROM user WHERE username = "admin"')
+            admin_user = cursor.fetchone()
+
+            default_admin_password = 'admin@1989'
+            
+            if admin_user:
+                if argon2.check_password_hash(admin_user['password'], default_admin_password):
+                    return True
+                
+            return create_defaultAdmin(default_admin_password)
+    except Exception as e:
+        print(f"Search Admin: {e}")
--- serve.py
+++ serve.py
@@ -1,6 +1,14 @@
 from waitress import serve
 from app import app  # Import the Flask app instance
+from app.secure.default_account import generateAdmin
 
 if __name__ == "__main__":
+    if not generateAdmin():
+        print('default admin exist')
+    else:
+        print('created default admin')
+        
     serve(app, host='127.0.0.1', port=8080)
+
+
 
